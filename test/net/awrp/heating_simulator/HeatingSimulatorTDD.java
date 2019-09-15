24b95c5e94650d04d4b8df907de734b70fbe180e
Anders Petersen <AndP@easj.dk>
2019-09-15 15:43:30 +0200

    Initial test list added to TDD class

--- test/net/awrp/heating_simulator/HeatingSimulatorTDD.java
+++ test/net/awrp/heating_simulator/HeatingSimulatorTDD.java
@@ -4,4 +4,58 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HeatingSimulatorTDD {
 
+    /*********************************************************\
+     TEST LIST
+
+     Create heating unit
+     Get maximum heating effect
+     Get minimum heating effect
+     Set heating effect per hour
+     Read heating effect per hour
+
+     Create accumulation tank
+     Get volume
+     Get heat dissipation surface size
+     Get heating effect per hour
+     Set pressure
+     Get pressure
+     Add internal heating unit to accumulation tank
+     Get number of heating units
+     Get heating unit x
+     Read number of measuring points
+     Read temperature of measuring point x
+
+     Create circulation pump
+     Get maximum flow rate
+     Get minimum flow rate
+     Set flow rate per hour
+     Get flow rate per hour
+
+     Create radiator
+     Get heat dissipation surface size
+     Get heating effect per hour
+
+     Create room
+     Get room size
+     Get heat loss per hour
+     Get room temperature
+     Add radiator
+     Read number of radiators
+     Get radiator x
+
+     Create house
+     Add room
+     Read number of rooms
+     Get room x
+     Get house size
+     Get heat loss per hour
+     Get average temperature
+
+     Notes:
+        Heating effect is measured in kW
+        Flow rate is measured in m3/hour
+        Surface and room sizes are measured in m2
+        Temperature is measured in degrees Celsius
+     \*********************************************************/
+
 }
